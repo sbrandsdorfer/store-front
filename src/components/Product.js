da649459bdea33312cf38243bd263dd070a7d1cc
Shimon Brandsdorfer <sbrandsorfer@viewpointlearn.com>
2023-03-05 15:53:44 -0500

    css updates

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,22 +7,32 @@ const Product = () => {
             src="./images/choc-mask.jpg"
         />
     </div>
-    <div style={{'display': 'flex', 'flex-direction': 'column'}}>
+    <div style={ {display: 'flex', flexDirection: 'column'} }>
         <div><h3>Chocolate Mask</h3></div>
         <div><p>Handmade Belgian Dark Chocolate Mask Pop</p></div>
-        <div style={{'flex-grow': 1}}><b>$3.25</b></div>
+        <div style={ {flexGrow: 1} }><b>$3.25</b></div>
         <div
             style={{
-                'display': 'flex',
-                'flex-direction': 'row-reverse',
-                'flex-shrink': 1
+                display: 'flex',
+                flexDirection: 'row-reverse',
+                flexShrink: 1
             }}
         >
-            <div className="plus"><div>+</div></div>
-            <div style={{'margin': '0.4rem 0.5rem 0'}}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                style={{'width': 'calc(8 * var(--unit))'}}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <div style={{margin: '0.4rem 0.5rem 0'}}>
                 <b>1</b>
             </div>
-            <div className="minus"><div>-</div></div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{'width': 'calc(8 * var(--unit))'}}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
         </div>
     </div>
 </li>
